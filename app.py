f447a00d43fc1aa56c1cbab3a83870341f6fa491
UCHIHA-MADRA <prabhuchaturvedi28@gmail.com>
2023-06-20 14:54:58 +0530

    Running

--- app.py
+++ app.py
@@ -9,13 +9,14 @@
 #     app.run(debug=True)
 
 
-from flask import Flask
+from flask import Flask , render_template
 app = Flask(__name__)
 
 app.debug = True
 
 @app.route('/')
 def hello_world():
-    return 'Hello World!'
+    # return 'Hello World!'
+    return render_template('home.html')
 if __name__ == '__main__':
     app.run(debug=True, port=5001)
